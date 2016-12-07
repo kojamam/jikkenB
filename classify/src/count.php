99a1c43c8d4fc9e85d9ebc9840282355e7384f15
tcyyky <developer.koji@gmail.com>
2016-12-07 13:30:35 +0900

    後半開始

---
+++ classify/src/count.php
@@ -0,0 +1,21 @@
+<?php
+
+$doc = [];
+
+// 読み込み
+while($line = trim(fgets(STDIN))){
+    $morp = explode("\t", $line);
+
+    //TODO 同音異義語への対応
+    if($morp[0] != "EOS"){
+        isset($doc[$morp[0]]) ? $doc[$morp[0]]++ : $doc[$morp[0]] = 1;
+    }
+}
+
+// 降順ソート
+arsort($doc);
+
+// 表示
+foreach ($doc as $morp => $count) {
+    print("$morp\t$count\n");
+}
